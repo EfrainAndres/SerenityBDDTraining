8044454ec0b6944f823825fc7273f01eef875d90
unknown <efrainvergara.udec@gmail.com>
2021-01-17 23:40:28 -0500

    Parte 6

--- src/test/java/SerenityBddTests.java
+++ src/test/java/SerenityBddTests.java
@@ -1,13 +1,16 @@
+import models.users.Datum;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import questions.GetUsersQuestion;
 import questions.ResponseCode;
 import tasks.GetUsers;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 @RunWith(SerenityRunner.class)
 public class SerenityBddTests {
@@ -21,12 +24,24 @@ public class SerenityBddTests {
                 .whoCan(CallAnApi.at(restApIUrl));
 
         julian.attemptsTo(
-            GetUsers.fromPage(2)
+            GetUsers.fromPage(1)
         );
 
         //assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
         julian.should(
                 seeThat("el codigo de respuesta", ResponseCode.was(), equalTo(200))
+        );
+
+        Datum user = new GetUsersQuestion().answeredBy(julian)
+                .getData().stream().filter(x -> x.getId() == 1).findFirst().orElse(null);
+
+        julian.should(
+                seeThat("usuario no es nulo", act ->  user, notNullValue())
+        );
+
+        julian.should(
+                seeThat("El email del usuario", act ->  user.getEmail(), equalTo("george.bluth@reqres.in")),
+                seeThat("El avatar del usuario", act ->  user.getAvatar(), equalTo("https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"))
         );
     }
 }
