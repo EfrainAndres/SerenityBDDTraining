703aa8e2132fdce7f0d61e9e79512bfb3807e8d3
efrain-vergara <efrain.vergara@avaldigitallabs.com>
2021-01-25 16:17:54 -0500

    Add changes to cucumber

---
+++ src/main/java/com/testautomation/mesaj/stepdefinitions/registerUserStepDefiniton.java
@@ -0,0 +1,45 @@
+package com.testautomation.mesaj.stepdefinitions;
+
+import com.testautomation.mesaj.questions.ResponseCode;
+import com.testautomation.mesaj.tasks.RegisterUser;
+
+import io.cucumber.java.en.*;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class registerUserStepDefiniton {
+
+    private final String restApIUrl = "http://localhost:5000/api";
+    Actor efrain;
+
+    @Given("Efrain es un cliente que quiere poder administrar sus productos bancarios")
+    public void efrainEsUnClienteQueQuierePoderAdministrarSusProductosBancarios() {
+        efrain = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApIUrl));
+    }
+
+    @When("el envia la informacion requerida para el registro")
+    public void elEnviaLaInformacionRequeridaParaElRegistro() {
+        String registerUserInfo = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\",\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"pistol\"\n" +
+                "}";
+
+        efrain.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+        );
+    }
+
+    @Then("el debe obtener una cuenta virtual para poder ingresar cuando lo requiera")
+    public void elDebeObtenerUnaCuentaVirtualParaPoderIngresarCuandoLoRequiera() {
+        efrain.should(
+                seeThat("El codigo de respuesta", ResponseCode.was(), equalTo(200))
+        );
+    }
+
+}
---
+++ src/test/java/com/testautomation/mesaj/SerenityBddTests.java
@@ -0,0 +1,133 @@
+package com.testautomation.mesaj;
+
+import com.testautomation.mesaj.facts.NetflixPlans;
+import com.testautomation.mesaj.models.users.Datum;
+import com.testautomation.mesaj.models.users.RegisterUserInfo;
+import com.testautomation.mesaj.models.users.UpdateUserInfo;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.testautomation.mesaj.questions.GetUsersQuestion;
+import com.testautomation.mesaj.questions.ResponseCode;
+import com.testautomation.mesaj.tasks.GetUsers;
+import com.testautomation.mesaj.tasks.RegisterUser;
+import com.testautomation.mesaj.tasks.UpdateUser;
+
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.contains;
+
+@RunWith(SerenityRunner.class)
+public class SerenityBddTests {
+
+    private static final String restApIUrl = "http://localhost:5000/api";
+
+    @Test
+    public void initialTest(){
+
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApIUrl));
+
+        julian.attemptsTo(
+            GetUsers.fromPage(1)
+        );
+
+        //assertThat(SerenityRest.lastResponse().statusCode()).isEqualTo(200);
+        julian.should(
+                seeThat("el codigo de respuesta", ResponseCode.was(), equalTo(200))
+        );
+
+        Datum user = new GetUsersQuestion().answeredBy(julian)
+                .getData().stream().filter(x -> x.getId() == 1).findFirst().orElse(null);
+
+        julian.should(
+                seeThat("usuario no es nulo", act ->  user, notNullValue())
+        );
+
+        julian.should(
+                seeThat("El email del usuario", act ->  user.getEmail(), equalTo("george.bluth@reqres.in")),
+                seeThat("El avatar del usuario", act ->  user.getAvatar(), equalTo("https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"))
+        );
+    }
+
+    @Test
+    public void registerUserTest(){
+
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApIUrl));
+
+        String registerUserInfo = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\",\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"pistol\"\n" +
+                "}";
+
+        julian.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+        );
+
+        julian.should(
+                seeThat("El codigo de respuesta", ResponseCode.was(), equalTo(200))
+        );
+    }
+
+    @Test
+    public void registerUserTest2(){
+
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApIUrl));
+
+        RegisterUserInfo registerUserInfo = new RegisterUserInfo();
+
+        registerUserInfo.setName("morpheus");
+        registerUserInfo.setJob("leader");
+        registerUserInfo.setEmail("eve.holt@reqres.in");
+        registerUserInfo.setPassword("pistol");
+
+        julian.attemptsTo(
+                RegisterUser.withInfo(registerUserInfo)
+        );
+
+        julian.should(
+                seeThat("El codigo de respuesta", new ResponseCode(), equalTo(200))
+        );
+    }
+
+    @Test
+    public void UpdateUserTest(){
+
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApIUrl));
+
+        UpdateUserInfo updateUserInfo = new UpdateUserInfo();
+
+        updateUserInfo.setName("morpheus");
+        updateUserInfo.setJob("leader");
+
+        julian.attemptsTo(
+                UpdateUser.withInfo(updateUserInfo)
+        );
+
+        julian.should(
+                seeThat("El codigo de respuesta", ResponseCode.was(), equalTo(200))
+        );
+
+        restAssuredThat(lastResponse -> lastResponse.body(containsString("updatedAt")));
+
+    }
+
+    @Test
+    public void factTest(){
+
+        Actor julian = Actor.named("Julian the trainer")
+                .whoCan(CallAnApi.at(restApIUrl));
+
+        julian.has(NetflixPlans.toViewsSeries());
+    }
+
+}
